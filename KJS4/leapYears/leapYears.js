219d0e7296fdab4caf89cac9129ce0a715a830c5
kyriakos.kolokouris <kyriakoskolokouris@gmail.com>
2021-03-26 12:35:07 +0200

    2.leapYears

---
+++ KJS4/leapYears/leapYears.js
@@ -0,0 +1,11 @@
+const leapYears = function (year) {
+        if (year % 4 === 0 && year % 100 !== 0) {
+            return true;
+        } else if (year % 100 === 0 && year % 400 === 0) {
+            return true;
+        } else {
+            return false;
+}
+    }
+
+module.exports = leapYears
