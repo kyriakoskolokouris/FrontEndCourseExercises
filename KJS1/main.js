815f362679761c5981f2bcb0ed120ea1c8ba02b7
kyriakos.kolokouris <kyriakoskolokouris@gmail.com>
2021-03-26 11:05:43 +0200

    Fundamentals 1

---
+++ KJS1/main.js
@@ -0,0 +1,21 @@
+
+        discountPerCent = 30;
+        let discount = discountPerCent / 100;
+         console.log(discount) ;
+         price1 = 666.67;
+         let finalPrice1 = price1 * -(discount - 1);
+         console.log(~~finalPrice1);
+         
+         price2 = 534;
+         let finalPrice2 = price2 * -(discount - 1);
+         console.log(~~finalPrice2);
+
+         price3 = 467;
+         let finalPrice3 = price3 * -(discount - 1);
+         console.log(~~finalPrice3);
+
+         price4 = 1667;
+         let finalPrice4 = price4 * -(discount - 1);
+         console.log(~~finalPrice4);
+
+       
