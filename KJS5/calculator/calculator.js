2efbfe7ac1122a54d3981ac20b591d9b74036f55
kyriakos.kolokouris <kyriakoskolokouris@gmail.com>
2021-04-23 20:30:58 +0300

    calculator

--- KJS5/calculator/calculator.js
+++ KJS5/calculator/calculator.js
@@ -1,29 +1,42 @@
-function add (a, b) {
+function add(a, b) {
 	return a + b;
 }
 
-function subtract (c, d) {
+function subtract(c, d) {
 	return c - d;
 }
 
-function sum (arr) {
-	reduce((a, b) => {
+function sum(arr) {
+	return arr.reduce((a, b) => {
 		return a + b;
-	}
+	}, 0)
 }
+
+
 
 function multiply (arr) {
-	arr.reduce((a, b) => 
-		a * b
-	)
-
-function power() {
+	return arr.reduce((a, b) => {
+		return a * b
 	
+})
 }
 
-function factorial() {
-	
+function power(arr) {
+	return Math.pow(4, 3);
 }
+
+function factorial(num) {
+	let result = num;
+  if (num === 0 || num === 1) 
+    return 1; 
+  while (num > 1) { 
+    num--;
+    result *= num;
+  }
+  return result;
+}
+	
+
 
 module.exports = {
 	add,
