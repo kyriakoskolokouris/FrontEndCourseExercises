2fdf1a722ec05ad76aa1d87528d9e8498ea215b9
kyriakos.kolokouris <kyriakoskolokouris@gmail.com>
2021-04-23 19:29:55 +0300

    2. ceasar

--- KJS5/caesar/caesar.js
+++ KJS5/caesar/caesar.js
@@ -7,13 +7,13 @@ const caesar = function (userInput, shiftNumber) {
 		if (shiftNumber < 0) {
 			shiftNumber = 26 + (shiftNumber % 26);
 		}
-
-		
+		// ASCII code for Uppercase letters
 		if (userInput.charCodeAt(i) >= 65 && userInput.charCodeAt(i) <= 90) {
 			const charCode = userInput.charCodeAt(i);
 			result += String.fromCharCode(((charCode - 65 + shiftNumber) % 26) + 65);
 
-			
+		// ASCII code for Lowercase letters
+
 		} else if (userInput.charCodeAt(i) >= 97 && userInput.charCodeAt(i) <= 122) {
 			const charCode = userInput.charCodeAt(i);
 			result += String.fromCharCode(((charCode - 97 + shiftNumber) % 26) + 97);
