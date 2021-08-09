1202c52e5524487e87aa79b92f5e678de2c09d85
kyriakos.kolokouris <kyriakoskolokouris@gmail.com>
2021-08-09 11:27:52 +0300

    constructors exercise

--- KJS5/palindromes/palindromes.js
+++ KJS5/palindromes/palindromes.js
@@ -1,9 +1,11 @@
 const palindromes = function(str) {
+
     const punctuation = /[^A-Za-z0-9]/g;
     const cleanedUp = str.toLowerCase().replace(punctuation, "");
     const reversed = cleanedUp.split("").reverse().join("");
 
     return cleanedUp === reversed;
-}
+};
+
 
 module.exports = palindromes
